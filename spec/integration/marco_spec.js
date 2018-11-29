887ccaafd1fc9595c9a73b7f3ea1c9a1904b6577
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-11-29 16:06:15 -0500

    Bloccit

--- spec/integration/marco_spec.js
+++ spec/integration/marco_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const base = "http://localhost:3000/marco";
 
 describe("routes : marco", () => {
 
@@ -8,16 +8,27 @@ describe("routes : marco", () => {
   describe("GET /", () => {
 
 //#2
-    it("should return status code 200", (done) => {
+      it("should return status code 200", (done) => {
+
+  //#3
+        request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+  //#4
+          done();
+    });
+});
+
+//#2
+      it("polo", (done) => {
 
 //#3
       request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
+      expect(body).toBe("polo");
 
 //#4
-        done();
-      });
+      done();
     });
-
+  });
   });
 });
--- src/routes/marco.js
+++ src/routes/marco.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
+router.get("/marco", (req, res, next) => {
+  res.send("polo");
 });
-
-
 
 module.exports = router;
