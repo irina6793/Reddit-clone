410c3520af77d739a033e965a23a508fa53887bb
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-11-29 11:43:37 -0500

    Bloccit

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const marcoRoutes = require("../routes/marco");
     app.use(staticRoutes);
+    app.use(marcoRoutes);
   }
 }
