052feb82c2dd19397ca1a0269e562a7c60f6b83a
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-15 14:40:14 -0500

    first

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,6 +1,6 @@
 module.exports = {
   index(req, res, next){
       res.render("static/index", {title: "Welcome to Bloccit"});
-      res.render("static/index", "About Us");
+      res.render('user', {name: "About Us"});
   }
 }
