1f10bf084bfedaceaa0ae9c77111c65bae3143cd
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-16 16:07:20 -0500

    first

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const topicRoutes = require("../routes/topics");
     const marcoRoutes = require("../routes/marco");
     app.use(staticRoutes);
+    app.use(topicRoutes);
     app.use(marcoRoutes);
   }
 }
