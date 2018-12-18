7154a26fca4fa2dc00d6a026744d0eb4a05a3c43
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-18 15:44:44 -0500

    first

--- src/db/migrations/20181218011612-create-rule.js
+++ src/db/migrations/20181218011612-create-rule.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        foreignKey:true,
+        
         type: Sequelize.INTEGER
       },
       source : {
--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -9,30 +9,8 @@ module.exports = (sequelize, DataTypes) => {
     Topic.hasMany(models.Banner, {
      foreignKey: "bannerId",
      as: "banners",
-     onDelete: "CASCADE",
-     validate: true
-    }).catch(errors => {
-      /* console.log(errors) would look like:
-      [
-        { record:
-        ...
-        name: 'SequelizeBulkRecordError',
-        message: 'Validation error',
-        errors:
-          { name: 'SequelizeValidationError',
-            message: 'Validation error',
-            errors: [Object] } },
-        { record:
-          ...
-          name: 'SequelizeBulkRecordError',
-          message: 'Validation error',
-          errors:
-            { name: 'SequelizeValidationError',
-            message: 'Validation error',
-            errors: [Object] } }
-      ]
-      */
-  });
+     onDelete: "CASCADE"
+    });
   return Topic;
 };
 }
