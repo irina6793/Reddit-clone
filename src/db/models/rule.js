409164fa61cada63a8168ba53683753ff6346c8f
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-17 22:44:32 -0500

    first

--- src/db/models/rule.js
+++ src/db/models/rule.js
@@ -1,14 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Rule = sequelize.define('Rule', {
-    description: DataTypes.STRING
-  });
+    topicId: DataTypes.STRING,
+    description: DataTypes.STRING,
+});
   Rule.associate = function(models) {
     // associations can be defined here
     Rule.belongsTo(models.Topic, {
-      foreignKey: "ruleId",
       foreignKey: "topicId",
-      as: "banners",
       onDelete: "CASCADE"
     });
   };
--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -7,7 +7,6 @@ module.exports = (sequelize, DataTypes) => {
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
-     foreignKey: "topicId",
      foreignKey: "ruleId",
      as: "banners",
      onDelete: "CASCADE"
