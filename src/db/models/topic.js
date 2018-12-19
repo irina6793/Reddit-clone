4ba08e95b7930cd4bdaf1c61499f9ab22b9c4ff8
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-19 12:03:20 -0500

    first

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,16 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    topicId: DataTypes.STRING,
+    description: DataTypes.TEXT
   });
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
      foreignKey: "bannerId",
-     as: "banners",
      onDelete: "CASCADE"
     });
+  }
   return Topic;
 };
-}
