7ca226b69bc4ea066273dbba2e57bb862b49b051
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-20 11:07:06 -0500

    first

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
     topicId: DataTypes.STRING,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    title: DataTypes.STRING
   });
   Topic.associate = function(models) {
     // associations can be defined here
