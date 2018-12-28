a25deb4595f9cfdac941439f81a01521103467ef
Irina <irina6793@yahoo.com>
2018-12-28 10:51:12 -0500

    post resource

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,10 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    topicId: DataTypes.STRING
-  });
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+     description: {
+       type: DataTypes.STRING,
+       allowNull: false
+    }
+  }, {});
+
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
@@ -14,7 +20,11 @@ module.exports = (sequelize, DataTypes) => {
     Topic.hasMany(models.Rule, {
       foreignKey: "topicId",
       as: "CASCADE"
-    })
-  }
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
+    });
+  };
   return Topic;
 };
