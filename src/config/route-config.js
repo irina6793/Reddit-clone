2c3cf2a660a17998e1ce8fc3abddcb7c730997a3
Irina <irina6793@yahoo.com>
2018-12-30 21:40:46 -0500

    updates

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
+    const postRoutes = require("../routes/posts");
     const topicRoutes = require("../routes/topics");
     const advertisementRoutes = require("../routes/advertisement");
     const marcoRoutes = require("../routes/marco");
 
     app.use(staticRoutes);
+    app.use(postRoutes);
     app.use(topicRoutes);
     app.use(advertisementRoutes);
     app.use(marcoRoutes);
---
+++ src/controllers/postController.js
@@ -0,0 +1,3 @@
+module.exports = {
+}
+const postQueries = require("../db/queries.posts.js");
