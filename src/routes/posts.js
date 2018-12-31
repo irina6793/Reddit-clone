1e9257aa3c250e68c849a2f37e3aab8075c39c5c
Irina <irina6793@yahoo.com>
2018-12-31 14:55:02 -0500

    updates

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const postController = require("../controllers/postController")
 
 router.get("/topics/:topicId/posts/new", postController.new);
-
-
-
+router.post("/topics/:topicId/posts/create", postController.create);
 
 module.exports = router;
