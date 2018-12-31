d103a9822b1a7eb549f639434cdf47b6c9435ba4
Irina <irina6793@yahoo.com>
2018-12-30 20:47:54 -0500

    updates

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,5 +1,5 @@
 const Topic = require("./models").Topic;
-
+const Post = require("./models").Post;
 
 module.exports = {
 
@@ -18,6 +18,13 @@ module.exports = {
 
   getTopic(id, callback){
     return Topic.findById(id)
+
+    return Topic.findById(id, {
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
    .then((topic) => {
      callback(null, topic);
   })
