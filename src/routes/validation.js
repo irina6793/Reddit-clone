9228ef56a02c0360bc659446b90887b6a47ff75c
Irina <irina6793@yahoo.com>
2019-01-07 21:40:13 -0500

    updates

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -21,5 +21,29 @@ return res.redirect(303, req.headers.referer)
 } else {
   return next();
   }
+},
+
+validateTopic(req, res, next) {
+
+ //#1
+ if(req.method === "POST") {
+
+ //#2
+ req.checkParams("topicId", "must be valid").notEmpty().isInt();
+ req.checkDescription("title", "must be at least 5 characters in length").isLength({min: 5});
+ req.checkDescription("description", "must be at least 10 characters in length").isLength({min: 10});
+ }
+
+ //#3
+ const errors = req.validationErrors();
+
+ if (errors) {
+
+ //#4
+ req.flash("error", errors);
+ return res.redirect(303, req.headers.referer)
+ } else {
+ return next();
+ }
  }
 }
