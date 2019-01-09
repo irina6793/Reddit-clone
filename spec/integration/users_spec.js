1035e238055bd54ededc2fe37fadf9b3440de875
Irina <irina6793@yahoo.com>
2019-01-08 20:45:29 -0500

    updates

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -83,4 +83,14 @@ it("should not create a new user with invalid attributes and redirect", (done) =
   );
 });
 });
+
+describe("GET /users/sign_in", () => {
+  it("should render a view with a sign in form", (done) => {
+    request.get(`${base}sign_in`, (err, res, body) => {
+      expect(err).toBeNull();
+      expect(body).toContain("Sign in");
+      done();
+    });
+  });
+});
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,6 +6,8 @@ const userController = require("../controllers/userController")
 
 router.get("/users/sign_up", userController.signUp);
 router.post("/users", validation.validateUsers, userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
 
 
 
