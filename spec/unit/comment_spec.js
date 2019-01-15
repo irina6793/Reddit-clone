980dcad05adce3eb6bfc8421a5d3cfcd05e37d93
Irina <irina6793@yahoo.com>
2019-01-15 11:59:12 -0500

    updates

--- spec/unit/comment_spec.js
+++ spec/unit/comment_spec.js
@@ -25,3 +25,41 @@ describe("Comment", () => {
 
       Topic.create({
         title: "Expeditions to Alpha Centauri",
+        description: "A compilation of reports from recent visits to the star system.",
+        posts: [{
+          title: "My first visit to Proxima Centauri b",
+          body: "I saw some rocks.",
+          userId: this.user.id
+        }]
+      }, {
+        include: {
+          model: Post,
+          as: "posts"
+        }
+      })
+      .then((topic) => {
+        this.topic = topic;
+        this.post = this.topic.posts[0];
+
+      Comment.create({
+        body: "ay caramba!!!!!",
+        userId: this.user.id,
+        postId: this.post.id
+      })
+      .then((comment) => {
+        this.comment = comment;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+   });
+  });
+ });
+});
