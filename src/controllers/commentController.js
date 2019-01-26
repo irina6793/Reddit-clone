23fbeb0c94c446a92702aba771f49044d8dffd0e
Irina <irina6793@yahoo.com>
2019-01-26 11:34:05 -0500

    fav

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,10 +3,11 @@ const Authorizer = require("../policies/comment.js");
 
 module.exports = {
   create(req, res, next){
-
+    console.log("Creating comment with user:");
+        console.log(req.user);
     const authorized = new Authorizer(req.user).create();
     if(authorized) {
-
+      console.log("User is authorized");
       let newComment = {
         body: req.body.body,
         userId: req.user.id,
@@ -16,11 +17,15 @@ module.exports = {
       commentQueries.createComment(newComment, (err, comment) => {
 
         if(err){
+                    console.log("There was an error while creating the comment:");
+                              console.log(err);
           req.flash("error", err);
         }
+                console.log("Redirecting to referrer");
         res.redirect(req.headers.referrer);
       });
     } else {
+            console.log("User is not authorized");
       req.flash("notice", "You must be signed in to do that.")
       req.redirect("/users/sign_in");
     }
